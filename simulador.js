8ae9004e72041b3a4ac3aa755a9b5803978f2337
Kel <kduranmalave@gmail.com>
2022-03-01 21:11:39 -0300

    SE agregan arrays

--- simulador.js
+++ simulador.js
@@ -13,23 +13,28 @@ let suma = 0;
 const calcularPromedio = (total, conteoDeCalificaciones) => {
     return total/conteoDeCalificaciones
 }
-
-while (true) {
-    entrada = prompt('Dame una calificacion (listo para terminar)')
-    if (entrada == 'listo') {
-        break
+class Grade{
+    constructor(name, score){
+        this.name = name
+        this.score = score
     }
-    
-    if(parseInt(entrada) >= 0 && parseInt(entrada) <= 100) {
-        contador++
-        suma = suma + parseInt(entrada)
-    } else {
-        alert('Calificacion no valida')
-    }
-
-    
 }
 
+const subjects = ['Matematicas', 'Fisca', 'Quimica', 'Contabilidad', 'Finanzas']
+let grades = []
+
+for (const subject of subjects) {
+    let score = prompt (`Calificacion de ${subject}`)
+    grades.push(new Grade(subject, score))
+    
+    if(parseInt(score) >= 0 && parseInt(score) <= 100) {
+        contador++
+        suma = suma + parseInt(score)
+    } else {
+        alert('Calificacion no valida, ingresa las notas de nuevo')
+        prompt (`Calificacion de ${subject}`) 
+    } 
+}
 console.log('El total de tus calificaciones es:', suma);
 console.log('Tu promedio es:', calcularPromedio(suma, contador))
 
@@ -38,3 +43,5 @@ if (calcularPromedio(suma,contador) < 50) {
 } else{
   console.log('Felicidades, has aprobado el curso')      
 }
+
+console.log(grades)
